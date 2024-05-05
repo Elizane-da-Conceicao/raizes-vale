07d87f0f61cc29804634333529744b19dfb8f613
Liz <elizanedaconceicao@gmail.com>
2024-05-05 19:47:28 -0300

    Alteração header e footer

--- frontend/includes/footer.php
+++ frontend/includes/footer.php
@@ -1,7 +1,7 @@
 </main>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <footer>
-    <p>&copy; 2024 Raizes do Vale</p>
+    <!-- <p>&copy; 2024 Raizes do Vale</p> -->
 </footer>
 </body>
 </html>
--- frontend/includes/header.php
+++ frontend/includes/header.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <!-- <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet"> -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <title><?php echo $pageTitle; ?></title>
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
@@ -13,11 +14,7 @@
         <!-- <img src="assets/img/logo.png" alt="Logo"> -->
     </div>
     <nav>
-        <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Sobre</a></li>
-            <li><a href="#">Contato</a></li>
-        </ul>
+        
     </nav>
 </header>
 <main>
